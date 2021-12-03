3ae84987af95229949e5a09fc9aca78d45e19f00
Sebastian Hoffmann <sebastianhoffmann@REM-SHOFFMANN-LT.localdomain>
2021-12-03 08:50:30 -0800

    Day 3 Binary Diagnostic part 2

--- Day3BinaryDiagnostic.ex
+++ Day3BinaryDiagnostic.ex
@@ -2,6 +2,24 @@ defmodule Day3BinaryDiagnostic do
   def gamma(list), do: get_value(list, 0)
   def delta(list), do: get_value(list, 1)
 
+  def oxygen(list) do
+    find_binary(list, 0, &most_common_value/2)
+      |> binary_list_to_int
+  end
+
+  def co2(list) do
+    find_binary(list, 0, &least_common_value/2)
+      |> binary_list_to_int
+  end
+
+  defp find_binary([item], _, _), do: item
+  defp find_binary(list, level, value_selector) do
+    value = value_selector.(list, level)
+    list
+      |> Enum.filter(fn x -> Enum.at(x, level) == value end)
+      |> find_binary((level + 1), (value_selector))
+  end
+
   defp get_value(list, value) do
     width = Enum.count(Enum.at(list, 0))-1
 
@@ -10,6 +28,18 @@ defmodule Day3BinaryDiagnostic do
     |> Enum.map(fn n -> (n > (Enum.count(list)/2)) end)
     |> Enum.map(&boolean_to_integer/1)
     |> binary_list_to_int
+  end
+
+  defp most_common_value(list, position) do
+    zero = count(list, position, 0)
+    one = count(list, position, 1)
+    if (zero > one), do: 0, else: 1
+  end
+
+  defp least_common_value(list, position) do
+    zero = count(list, position, 0)
+    one = count(list, position, 1)
+    if (zero > one), do: 1, else: 0
   end
 
   defp count(list, column, value) do
@@ -44,3 +74,7 @@ file =
   gamma = Day3BinaryDiagnostic.gamma(file)
   delta = Day3BinaryDiagnostic.delta(file)
   IO.puts(to_string(gamma) <> " * " <> to_string(delta) <> " = " <> to_string(gamma*delta)) # 2921 * 1174 = 3429254
+
+  oxygen = Day3BinaryDiagnostic.oxygen(file)
+  co2 = Day3BinaryDiagnostic.co2(file)
+  IO.puts(to_string(oxygen) <> " * " <> to_string(co2) <> " = " <> to_string(oxygen*co2)) # 1471 * 3678 = 5410338
